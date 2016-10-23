ffd41b30d3ec3d50e272acec623ee9e9e6374d27
mayeg <mayegarciadiaz@gmail.com>
2016-10-23 08:48:47 -0500

    +add admin.py  models

--- mincit/admin.py
+++ mincit/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from models import Diagnostico_Emp, Recurso
+from models import Informacion, Direccion
+from models import Situacion, Control
+from models import Planeacion, Organizacion
 
 # Register your models here.
+admin.site.register(Diagnostico_Emp)
+admin.site.register(Informacion)
+admin.site.register(Situacion)
+admin.site.register(Planeacion)
+admin.site.register(Organizacion)
+admin.site.register(Recurso)
+admin.site.register(Direccion)
+admin.site.register(Control)
+
+
+
+
+
