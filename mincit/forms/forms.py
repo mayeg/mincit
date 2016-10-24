dbece1382e65c2bace5b048ac3182a8ada07314e
mayeg <mayegarciadiaz@gmail.com>
2016-10-24 06:18:48 -0500

    add informacion in forms.py

--- mincit/forms/forms.py
+++ mincit/forms/forms.py
@@ -1,10 +1,33 @@
 
+
 from django import forms
+from mincit.models import Informacion
 
 
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=20)
     password = forms.CharField(max_length=20, widget=forms.PasswordInput())
+
+
+class InformacionForm(forms.ModelForm):
+    class Meta:
+        model = Informacion
+        fields = (
+            'nombre_empresa',
+            'razon_social',
+            'nombre_contacto',
+            'posicion_empresa',
+            'numero_tel',
+            'telfono_movil',
+            'tiempo_operacion',
+            'productos_servicio')
+
+
+
+
+
+
+
 
 
 
--- mincit/views.py
+++ mincit/views.py
@@ -3,9 +3,12 @@ from django.contrib.auth import login as login_django
 from django.contrib.auth import logout as logout_django
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render, redirect
+from django.template.context_processors import request
+from django.core.urlresolvers import reverse_lazy
 
-from mincit.forms.forms import LoginForm
+from mincit.forms.forms import LoginForm, InformacionForm
 from django.views.generic import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -61,7 +64,17 @@ class Diagnostico_empViews(LoginRequiredMixin, View):
 
 class InformacionViews(LoginRequiredMixin, View):
     login_url = 'mincit:login'
-
+    success_url = reverse_lazy('client:situacion')
+    template = 'informacion.html'
+    form = InformacionForm(request.POST or None)
+    context = {
+        'form': form
+    }
 
     def get(self, request, *args, **kwargs):
-        return render(request, 'informacion.html', {})
+        return render(request, self.template, self.context)
+
+    def post(self, request, *args, **kwargs):
+        if self.form.is_valid():
+            self.form.save()
+        return HttpResponseRedirect(self.get_success_url())
